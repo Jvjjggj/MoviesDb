efa20c4564d37e7819a58efb44e404bca4deba5d
Shaik Jakeer Hussain <127141771+Jvjjggj@users.noreply.github.com>
2023-08-17 18:33:12 +0530

    completed upto API 7

--- app.js
+++ app.js
@@ -23,16 +23,26 @@ const connetDBAndServer = async () => {
 
 connetDBAndServer();
 
+const directorFormat = function (i) {
+  return {
+    directorId: i.director_id,
+    directorName: i.director_name,
+  };
+};
+// API 1
 app.get("/movies/", async (request, response) => {
   const dbQuery = `select * from movie`;
   const dbresponse = await db.all(dbQuery);
   let lst = [];
   for (let i of dbresponse) {
-    lst.push(i.movie_name);
+    lst.push({
+      movieName: i.movie_name,
+    });
   }
-  response.send(lst);
+  response.send(dbresponse);
 });
 
+// API 2
 app.post("/movies/", async (request, response) => {
   const movieDetails = request.body;
   //https://github.com/Jvjjggj/MoviesDb.git
@@ -46,30 +56,54 @@ app.post("/movies/", async (request, response) => {
   response.send("Movie Successfully Added");
 });
 
+// API 3
 app.get("/movies/:movieId/", async (request, response) => {
   const { movieId } = request.params;
-  const integer = Number(movieId.movieId);
-  const getBookQuery = `
-    SELECT
-        *
-    FROM
-        movie
-    WHERE
-        movie_id=${movieId};
-    `;
-
-  const dbresponse = await db.get(getBookQuery);
-
-  const change = (dbresponse) => {
-    return {
-      movieId: dbresponse.movie_id,
-      directorId: dbresponse.director_id,
-      movieName: dbresponse.movie_name,
-      leadActor: dbresponse.lead_actor,
-    };
-  };
-
-  const changedb = change(dbresponse);
-
-  response.send(changedb);
+  const query = `
+  select * from movie 
+  where movie_id=${movieId}`;
+  const dbresponse = await db.get(query);
+  response.send(dbresponse);
 });
+
+// API 4
+
+app.put("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const movieDetails = request.body;
+  const { directorId, movieName, leadActor } = movieDetails;
+  const query = `
+  update  movie 
+  set director_id=${directorId} ,movie_name="${movieName}" ,lead_actor="${leadActor}" ;`;
+
+  const dbresponse = await db.run(query);
+  response.send("Movie Details Updated");
+});
+
+// API 5
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const query = `
+    delete from movie 
+    where movie_id=${movieId}`;
+  const dbresponse = await db.run(query);
+  response.send("Movie Removed");
+});
+
+// API 6
+app.get("/directors/", async (request, response) => {
+  const query = `
+    select * from 
+    director`;
+  const dbresponse = await db.all(query);
+  const lst = [];
+  for (let i of dbresponse) {
+    const change = directorFormat(i);
+    lst.push(change);
+  }
+  response.send(lst);
+});
+
+// API 7
+
+module.exports = app;
